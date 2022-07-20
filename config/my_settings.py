13fa63c89e2f690d89ae8f1f43263b335106cee0
김훈희 <nmdkims@gmail.com>
2022-07-20 10:41:49 +0900

    Hotfix : #1 SECRET 키 분리
    - SECRET키 변경하여 다른 파일로 분리

---
+++ config/my_settings.py
@@ -0,0 +1,1 @@
+MY_SECRET = 'django-insecure-%+1zfor_i8nr_(&+^%jiur^=s-131@!pdq%g07ve%n^e9%&ausg3'
--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from .my_settings import MY_SECRET
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%+zfor_i8nr_(&+-^%jiur^=s-g@!pdq%g07ve%n^e9%&ausg3'
+SECRET_KEY = MY_SECRET
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
