25dbfa05c4f28d2db628d1735ef1ac40d7b08870
김훈희 <nmdkims@gmail.com>
2022-07-21 14:37:12 +0900

    Style : 형식 동기화

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
 
 from .views import LoginView, LogoutView, UserSignupApiView
 
